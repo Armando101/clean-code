e85187d79144fcef3bbec655a12c644d4fd7225a
Armando Rivera <rivera.armando997@gmail.com>
2022-05-16 15:10:48 -0500

    cc-td: homework functions

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import './style.css'
+import "./style.css";
+import "./clean-code/04-homework";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
-`
-
+`;
