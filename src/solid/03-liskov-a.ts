d7174e199ab3eba493514cc1d075768fde24264a
Armando Rivera <rivera.armando997@gmail.com>
2022-05-19 23:42:08 -0500

    solid-liskov: solución al problema

--- src/solid/03-liskov-a.ts
+++ src/solid/03-liskov-a.ts
@@ -1,25 +1,29 @@
-import { Tesla, Audi, Toyota, Honda } from "./03-liskov-b";
+import { Tesla, Audi, Toyota, Honda, Vehicle } from "./03-liskov-c";
 
 (() => {
-  const printCarSeats = (cars: (Tesla | Audi | Toyota | Honda)[]) => {
-    for (const car of cars) {
-      if (car instanceof Tesla) {
-        console.log("Tesla", car.getNumberOfTeslaSeats());
-        continue;
-      }
-      if (car instanceof Audi) {
-        console.log("Audi", car.getNumberOfAudiSeats());
-        continue;
-      }
-      if (car instanceof Toyota) {
-        console.log("Toyota", car.getNumberOfToyotaSeats());
-        continue;
-      }
-      if (car instanceof Honda) {
-        console.log("Honda", car.getNumberOfHondaSeats());
-        continue;
-      }
-    }
+  const printCarSeats = (cars: Vehicle[]) => {
+    cars.forEach((car) => {
+      console.log(car.constructor.name, car.getNumberOfSeats());
+    });
+
+    // for (const car of cars) {
+    //   if (car instanceof Tesla) {
+    //     console.log("Tesla", car.getNumberOfSeats());
+    //     continue;
+    //   }
+    //   if (car instanceof Audi) {
+    //     console.log("Audi", car.getNumberOfSeats());
+    //     continue;
+    //   }
+    //   if (car instanceof Toyota) {
+    //     console.log("Toyota", car.getNumberOfSeats());
+    //     continue;
+    //   }
+    //   if (car instanceof Honda) {
+    //     console.log("Honda", car.getNumberOfSeats());
+    //     continue;
+    //   }
+    // }
   };
 
   const cars = [new Tesla(7), new Audi(2), new Toyota(5), new Honda(5)];
